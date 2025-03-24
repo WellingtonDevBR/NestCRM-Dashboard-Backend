ccef7b26212bb61e329efc4cd79ec1d78fa4ab0e
Wellington <wellington.sydney1@gmail.com>
2025-03-24 20:57:05 +1100

    fix import

--- src/interfaces/controllers/customerController.ts
+++ src/interfaces/controllers/customerController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { DynamoCustomerRepository } from "../../infrastructure/repositories/dynamoCustomerRepository";
-import { CustomerUseCase } from "../../application/usecases/CustomerUseCase";
+import { CustomerUseCase } from "../../application/usecases/customerUseCase";
 
 const repository = new DynamoCustomerRepository();
 const useCase = new CustomerUseCase(repository);
